b14c5f287388b7a51fca3d459ab9ee4bd165320f
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-03-31 02:17:14 -0400

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def app():
-    st.title ("Home")
+    st.title ("Home Page")
 
-    st.write("This is the home page")
+    st.write("This is the landing page, you can use the drop down menu to select the application you are interested in.")
