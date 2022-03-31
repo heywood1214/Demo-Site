5c6d4a73675a117c7651ed199e10a1c8a819cf69
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-03-31 02:16:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = MultiApp()
 
 st.markdown(""" 
 
-#Multi-Page App
+#Welcome to the Heywood Lau's portfolio site. 
 
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+This multi-page app can be used for personal finance and data analytics purposes. 
 
 
 """)
