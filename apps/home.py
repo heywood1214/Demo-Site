f79cdd268cdc548785faa680489ca87ec3dbc951
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-04-04 01:45:55 -0400

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -10,4 +10,4 @@ def app():
 
     #st.image("https://unsplash.com/photos/gHdP7B5XZ48")
 
-    st.image("https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGRhdGElMjBzY2llbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
+    st.image("https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGRhdGElMjBzY2llbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",use_column_width = 'always')
