1e74d80316eab43c10558f9ef59433fb9e6bc1fd
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-04-04 00:38:51 -0400

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
 def app():
-    st.title ("Home Page")
+    st.title ("Home")
 
-    st.write("This is the landing page, you can use the drop down menu to select the application you are interested in.")
+    st.header("Please feel free explore different projects utilizing real-time data")
+
+
+    st.write("This is the home page where you could find various practical calculators and data visualizations for predictions/analysis")
+
+    #st.image("https://unsplash.com/photos/gHdP7B5XZ48")
+
+    st.image("https://images.unsplash.com/photo-1620712943543-bcc4688e7485?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGRhdGElMjBzY2llbmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
