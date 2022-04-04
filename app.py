1e11d71a8d2b7a2d62908406fb12ee7257e17910
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-04-04 00:36:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import streamlit as st
 from multipage import MultiApp
-from apps import webapp_stock,home
+from apps import webapp_stock,home,retirement,SP500
 
 app = MultiApp()
 
-st.markdown(""" 
-
-#Welcome to the Heywood Lau's portfolio site. 
-
+st.subheader(""" 
+Welcome to the Heywood Lau's portfolio site. 
 This multi-page app can be used for personal finance and data analytics purposes. 
-
-
 """)
 
 #Add application here 
-
 app.add_app("Homepage", home.app)
-app.add_app("Stock/ETF Data Analysis", webapp_stock.app)
+app.add_app("Market and ETF Analysis", webapp_stock.app)
+app.add_app("Retirement Calculation", retirement.app)
+app.add_app("S&P 500/ETFs Linear Regression Prediction",SP500.app)
 
 
 app.run()
