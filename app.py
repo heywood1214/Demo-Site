88c88cbe2fe5a6ff4c4d9d35707db0402452f7f1
Heywood Lau <39145820+heywood1214@users.noreply.github.com>
2022-04-17 01:48:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multipage import MultiApp
-from apps import webapp_stock,home,retirement,SP500
+from apps import webapp_stock,home,retirement,SP500,portfolio_opt
 
 app = MultiApp()
 
@@ -10,10 +10,12 @@ This multi-page app can be used for personal finance and data analytics purposes
 """)
 
 #Add application here 
+app.add_app("Retirement Calculation", retirement.app)
 app.add_app("Homepage", home.app)
 app.add_app("Market and ETF Analysis", webapp_stock.app)
 app.add_app("Retirement Calculation", retirement.app)
 app.add_app("S&P 500/ETFs Linear Regression Prediction",SP500.app)
+app.add_app("Portfolio Optimization - build any portfolio from the Russell 2000 Index",portfolio_opt.app)
 
 
 app.run()
